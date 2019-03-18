771df6da2a6d7857051aaf2da730447b97e5b8d5
foreigner <starka@fit.vutbr.cz>
2019-03-18 13:33:58 +0100

    Testing nonreflective factory and other concepts

--- tests/main.cpp
+++ tests/main.cpp
@@ -32,6 +32,15 @@ public:
    }
 };
 
+class D
+{
+public:
+   int get()
+   {
+      return 3;
+   }
+};
+
 class NoDefaultCtor
 {
 public:
@@ -104,8 +113,236 @@ public:
    int b;
 };
 
+//
+class TypeProvider
+{
+public:
+   using ProvidedType = A;
+
+   virtual ~TypeProvider(){} //needed at least one virtual method to force the polymorfic type and VMT creation
+};
+
+class TypeProviderSub1 : public TypeProvider
+{
+public:
+   using ProvidedType = B;
+
+};
+
+class MyKey: public string
+{
+public:
+   MyKey() = delete;
+   MyKey(const std::type_info& ti)
+      :string(ti.name())
+   {}
+
+   friend 
+   ostream& operator<<(ostream& out, const MyKey& v)
+   {
+      return out << v.c_str();
+   }
+};
+
+
+template<typename _ReturnType, typename ...Arguments>
+class Functor
+{
+public:
+   using ReturnType = _ReturnType;
+
+   virtual ReturnType operator()(Arguments...args) = 0;
+};
+
+class I2I : public std::function<string(int)>
+{
+public:
+   I2I(string val)
+   : x(std::move(val))
+   {}
+      
+   virtual result_type operator()(int val) const
+   {
+      return x;
+   }
+protected:
+   string x;
+};
+
+class Increment : public Functor<Increment>
+{
+public:
+   int val;
+   Increment(int x=0)
+   : val(x)
+   {}
+   Increment operator()()override
+   {
+      return Increment{++val};
+   }
+
+   friend
+   ostream& operator<<(ostream& out, const Increment& v)
+   {
+      return out << v.val;
+   }
+};
+
+
+class Creator
+{
+public:
+   class Selector
+   {
+   public:
+
+      template<typename T>
+      I2I operator[](const T& id)
+      {
+         return I2I{typeid(id).name()};
+      }
+
+      /*I2I operator[](std::type_index&& id)
+      {
+         return I2I{id.name()};
+      }*/
+   };
+
+   Selector create;
+};
+
+class PDMSugar : fioc::Registry<std::map>
+{
+public:
+  //Selector& createPDMforProduct = create; //name alias
+};
+
+template<typename Base, typename SyntaxSugar>
+class SocketMixin : public Base, SyntaxSugar
+{};
+
+namespace fioc
+{
+
+template<typename _CommonType, typename T>
+class TemplateAgregatePOC
+{
+public:
+   typedef _CommonType CommonType;
+   typedef T Type;
+};
+
+template<typename  Agregate>
+class AgregateUser
+{
+public:
+   typedef typename Agregate::CommonType CT;
+   typedef typename Agregate::Type T;
+};
+
+
+template <template <typename ... > class _Map, typename _CommonType = void ,typename ...Args>
+class NonRefectiveRegistry
+{
+public:
+   using CommonType = _CommonType;
+   using Key = std::type_index;
+   using Value = std::unique_ptr<DefaultConstructorFunctor>;
+   using Map = _Map< Key, Value, Args ...>; //< The type of internal container (might come in handy)
+
+   template<typename BindType, typename ...Args>
+   CommonType* resolve(Args... args)
+   {
+      auto it = container.find(Key{typeid(BindType)});
+      if(it == container.end())
+      {
+         return nullptr;
+      }
+      FactoryFunctor<CommonType*, Args...> *factoryFunctor = static_cast<FactoryFunctor<CommonType*, Args...> *>(it->second.get());
+      factoryFunctor->arguments = std::make_tuple(args...);
+      return static_cast<CommonType*>((*it->second)());
+   }
+
+   template<typename CreatedType>
+   struct IntermediateReturn
+   {
+      IntermediateReturn(Map& map, std::unique_ptr<FactoryFunctor<CreatedType*>> ff)
+         : container(map)
+      {
+         factoryFunctor = std::move(ff);
+      }
+
+      template<typename BindType>
+      IntermediateReturn<CreatedType>& forType()
+      {
+         container.emplace(Key{typeid(BindType)},std::move(factoryFunctor));
+
+         return *this;
+      }
+
+      Map& container;
+      std::unique_ptr<FactoryFunctor<CreatedType*>> factoryFunctor;
+   };
+
+   template<typename CreatedType>
+   IntermediateReturn<CreatedType> registerType()
+   {
+      std::unique_ptr<FactoryFunctor<CreatedType*>> factoryFunctor{make_unique<FactoryFunctor<CreatedType*>>()};
+
+      factoryFunctor->f = []() { return new CreatedType(); };
+      //container.emplace(Key{typeid(T)}, factoryFunctor);
+
+      return IntermediateReturn<CreatedType>{container, std::move(factoryFunctor)};
+   }
+
+protected:
+   Map container; //< Map is e.g. std::map<std::string, std::function<void*()> >. container holds the factory functions
+};
+}
+
+
 int main(int argc, char* argv[])
 {
+   ///
+   TypeProvider tpr;
+   TypeProviderSub1 tps1;
+   TypeProvider *t = &tps1;
+   Creator creator;
+   std::type_index tid{typeid(*t)};
+   cout << MyKey{typeid(B)} << endl;
+   cout << "name " << typeid(*t).name() << endl;
+   //cout << "name " << decltype(t) << endl;
+   cout << "name " <<typeid(remove_pointer_t<decltype(*t)>).name() << endl;
+
+   ///
+   fioc::AgregateUser< fioc::TemplateAgregatePOC<A,B> > agrPOC;
+   cout << "Agregate " << typeid(decltype(agrPOC)::CT).name() << " " << typeid(decltype(agrPOC)::T).name() << endl;
+   ///
+   //Increment i(0);
+   cout << "increment " << Increment()()()()()() << endl;
+   ///
+
+   ///
+   /*PDMRegistry.registerPDM<PDM>().forProduct<Product>();
+   PDMRegistry.createPDMforProduct[product*](product*,viewer*);*/
+   ///
+
+
+
+   fioc::NonRefectiveRegistry<std::map> nrBuilder;
+   nrBuilder.registerType<C>().forType<B>();
+   nrBuilder.registerType<D>().forType<A>();
+   unique_ptr<A> nrResolved (static_cast<A*>(nrBuilder.resolve<B>()));
+
+   cout << "nrresolve " << nrResolved->get() << endl;
+
+   //nrResolved.reset( nrBuilder.resolve<A>());
+
+   //cout << "nrresolve " << nrResolved->get() << endl; //runtime access violation as expected
+
+   unique_ptr<D> nrr(static_cast<D*>(nrBuilder.resolve<A>()));
+   cout << "nrresolve " << nrr->get() << endl;
+   /////////////////////////////
    
    fioc::Registry<std::map /*,std::allocator<std::pair<std::string, std::function<void*()> > >*/ > builder;
    builder.registerType<A>();
